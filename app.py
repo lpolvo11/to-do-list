f3e0fb6abdc1239bfe0e2449898f6d71ad23c789
Outhman <124364506+lpolvo11@users.noreply.github.com>
2024-11-12 16:54:37 +0100

    commit message

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# The TaskManager class in Python manages a todo list stored in a PostgreSQL database, allowing users
-# to add, remove, update, and view tasks.
 import psycopg2
 import time
 
@@ -7,101 +5,93 @@ class TaskManager:
     def __init__(self, dbname, password, user, host, port):
         self.conn = psycopg2.connect(dbname=dbname, password=password, user=user, host=host, port=port)
         self.cursor = self.conn.cursor()
-        self.rename_table()
+        self.setup_database()
+
+    def setup_database(self):
         self.drop_table()
         self.create_table()
 
-    def rename_table(self):
-        try:
-            table_choice = input('1) to create a new todo list , 2) to work with the default new todo list: ')
-            if table_choice == '1':
-                new_table_name = input('Enter the new table name: ')
-                self.cursor.execute(f'''ALTER TABLE todo RENAME TO {new_table_name}''')
-            else:
-                self.cursor.execute('ALTER TABLE todo RENAME TO todo') 
-            self.commit()
-        except Exception as error:
-            print(f'Error while renaming the table, {str(error)}')
-            self.conn.rollback()
     def drop_table(self):
-        self.cursor.execute('''DROP TABLE IF EXISTS todo''')
+        self.cursor.execute('DROP TABLE IF EXISTS todo')
 
     def create_table(self):
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS todo (
-                            id SERIAL PRIMARY KEY,
-                            task_name CHAR(255)
-        )''')
-    
-    def add_task(self, task): 
-        self.cursor.execute('''INSERT INTO todo (task_name) VALUES (%s) RETURNING id''', (task,))
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS todo (
+                id SERIAL PRIMARY KEY,
+                task_name VARCHAR(255) NOT NULL
+            )
+        ''')
+        self.conn.commit()
+
+    def add_task(self, task):
+        if not task:
+            print("Task name cannot be empty.")
+            return
+        self.cursor.execute('INSERT INTO todo (task_name) VALUES (%s) RETURNING id', (task,))
         task_id = self.cursor.fetchone()[0]
         self.conn.commit()
         return task_id
-    def remove_task(self, id):
-        task_list = self.cursor.fetchall()
-        if not task_list:
-            print('the list is empty')
+
+    def remove_task(self, task_id):
+        self.cursor.execute('DELETE FROM todo WHERE id = %s', (task_id,))
+        if self.cursor.rowcount == 0:
+            print(f"No task found with ID {task_id}.")
         else:
-            self.cursor.execute('''DELETE FROM todo WHERE id = %s''', (id,))
             self.conn.commit()
-    def update_task(self, taskID, newTaskName):
-        try:
-            if newTaskName != '':
-                self.cursor.execute('''UPDATE todo SET task_name = %s WHERE id = %s''', (newTaskName, taskID))
-                self.conn.commit()
-            else:
-                print("Please enter a task name.")
-                print('Task updated successfully')
-        except Exception as error:
-            print(f'Error while updating the task,{str(error)}')
+            print(f"Task with ID {task_id} has been removed successfully.")
+
+    def update_task(self, task_id, new_task_name):
+        if not new_task_name:
+            print("Please enter a valid task name.")
+            return
+        self.cursor.execute('UPDATE todo SET task_name = %s WHERE id = %s', (new_task_name, task_id))
+        if self.cursor.rowcount == 0:
+            print(f"No task found with ID {task_id}.")
+        else:
+            self.conn.commit()
+            print(f"Task with ID {task_id} has been updated successfully.")
+
     def view_tasks(self):
         self.cursor.execute('SELECT * FROM todo')
-        tasks_seen = self.cursor.fetchall()
-        if not tasks_seen:
-            print('No tasks found')
+        tasks = self.cursor.fetchall()
+        if not tasks:
+            print('No tasks found.')
         else:
-            for task in tasks_seen:
-                print(f"The ID: {task[0]}, The task name: {task[1]}")
-            self.conn.commit()
-    def commit(self):
-        self.conn.commit()
+            for task in tasks:
+                print(f"ID: {task[0]}, Task: {task[1]}")
+
     def close(self):
+        self.cursor.close()
         self.conn.close()
 
-task_manager = TaskManager(dbname='todolist', password='123', user='postgres', host='localhost', port=8080)
-def main() -> any:
+def main():
+    task_manager = TaskManager(dbname='todolist', password='123', user='postgres', host='localhost', port=5432)
     while True:
-        user_choice = input("1) to add task , 2) to remove task , 3) to update task , 4) to view task , 5) to exit the program: \n")
+        user_choice = input("1) Add task, 2) Remove task, 3) Update task, 4) View tasks, 5) Exit: ")
         if user_choice == '1':
-            try:
-                addTask = str(input("Add task: "))
-                task_manager.add_task(addTask)
-                print(f"The '{addTask}' Task has been added successfully")
-            except Exception as error:
-                print(f"Error while adding the task,{str(error)}")
+            task_name = input("Enter task name: ")
+            task_manager.add_task(task_name)
         elif user_choice == '2':
             try:
-                remove_task = input("Remove Task ID: ")
-                task_manager.remove_task(remove_task)
-                print(f"The '{remove_task}' task has been removed successfully")
-            except Exception as error:
-                print(f"Error while removing the task,{str(error)}")
+                task_id = int(input("Enter Task ID to remove: "))
+                task_manager.remove_task(task_id)
+            except ValueError:
+                print("Please enter a valid integer for Task ID.")
         elif user_choice == '3':
-            updateTaskById = int(input('Enter the ID of the task you want to update: '))
-            newTaskName = input('Enter the new task name: ')
-            task_manager.update_task(updateTaskById, newTaskName)
-        elif user_choice == '4':
             try:
-                print('View tasks: ')
-                task_manager.view_tasks()
-                task_manager.commit()
-            except Exception as error:
-                print(f"Error while viewing the task,{str(error)}")
+                task_id = int(input("Enter Task ID to update: "))
+                new_task_name = input("Enter new task name: ")
+                task_manager.update_task(task_id, new_task_name)
+            except ValueError:
+                print("Please enter a valid integer for Task ID.")
+        elif user_choice == '4':
+            task_manager.view_tasks()
         elif user_choice == '5':
             task_manager.close()
             print('Goodbye!')
-            time.sleep(0.5)
-            exit()
+            break
         else:
-            print('please enter a valid choice.')
-main()
+            print('Invalid choice. Please try again.')
+
+if __name__ == "__main__":
+    main()
